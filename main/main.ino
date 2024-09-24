a5963c74bcb59d0af9aafb07d2dced8a9abf0809
phakin <oot.pakin@gmail.com>
2024-09-24 20:22:00 +0700

    fullnode

--- main/main.ino
+++ main/main.ino
@@ -2,10 +2,10 @@
 #include <Wire.h>
 #include <Adafruit_PCF8574.h>
 #include <SoftwareSerial.h>
-
-SoftwareSerial Uno(3, 2); // RX | TX
-Adafruit_PCF8574 pcf20;   // Initialize PCF8574 for first set of LEDs
-Adafruit_PCF8574 pcf21;   // Initialize PCF8574 for second set of LEDs
+#include <avr/sleep.h>
+SoftwareSerial Uno(3, 2);  // RX | TX
+Adafruit_PCF8574 pcf20;    // Initialize PCF8574 for first set of LEDs
+Adafruit_PCF8574 pcf21;    // Initialize PCF8574 for second set of LEDs
 
 // LED Pin Definitions for PCF8574 at addresses 0x20 and 0x3E
 #define ledAGreen 0
@@ -33,16 +33,22 @@ Adafruit_PCF8574 pcf21;   // Initialize PCF8574 for second set of LEDs
 #define echoPinD1 11
 
 void setup() {
+  pinMode(3, INPUT);
+  pinMode(2, OUTPUT);
+  
+  Uno.begin(9600);
   Serial.begin(9600);
 
   // Initialize PCF8574 at the specified addresses
   if (!pcf20.begin(0x20)) {
     Serial.println("Couldn't find PCF8574 at 0x20");
-    while (1);
+    while (1)
+      ;
   }
-  if (!pcf21.begin(0x3E)) {
-    Serial.println("Couldn't find PCF8574 at 0x3E");
-    while (1);
+  if (!pcf21.begin(0x21)) {
+    Serial.println("Couldn't find PCF8574 at 0x21");
+    while (1)
+      ;
   }
 
   // Set the LED pins as OUTPUT
@@ -50,78 +56,101 @@ void setup() {
     pcf20.pinMode(i, OUTPUT);
     pcf21.pinMode(i, OUTPUT);
   }
+  // Set the sensor pins as OUTPUT
+  pinMode(trigPinA1, OUTPUT);
+  pinMode(echoPinA1, INPUT);
+  
+  pinMode(trigPinB1, OUTPUT);
+  pinMode(echoPinB1, INPUT);
+
+  pinMode(trigPinC1, OUTPUT);
+  pinMode(echoPinC1, INPUT);
+
+  pinMode(trigPinD1, OUTPUT);
+  pinMode(echoPinD1, INPUT);
+  Serial.println("--start-- ");
+  //set on to OFF
+  setzero();
 }
 
 void DoNormalTraffic();  // Declare the function prototype
 
+void setzero(){
+  for (int i = 0; i <= ledBRed; i++) {
+    pcf20.digitalWrite(i, HIGH);
+    pcf21.digitalWrite(i, HIGH);
+  }
+  pcf20.digitalWrite(ledARed, LOW);
+  pcf20.digitalWrite(ledBRed, LOW);
+  pcf21.digitalWrite(ledCRed, LOW);
+  pcf21.digitalWrite(ledDRed, LOW);
+}
 void loop() {
+  
   int receivedValue = ReceiveData();  // Get the received value
-
-  if (receivedValue == 0) {
+  Serial.println(receivedValue);
+  if (receivedValue == 0 || receivedValue == -1) {
+    setzero();
     DoNormalTraffic();  // Handle normal traffic if no special command is received
   } else if (receivedValue > 0) {
     controlLEDs(receivedValue);  // Control LEDs based on the received value
   }
+  delay(1000);
 }
 
 int ReceiveData() {
   static String inputString = "";     // A string to hold incoming data
   while (Uno.available() > 0) {       // If there's incoming data
     char c = Uno.read();              // Read a character
-    if (c == '\n') {                  // If the character is a newline
-      int val = inputString.toInt();  // Convert the string to an integer
-      Serial.println(val);            // Print the value to Serial Monitor
-      inputString = "";               // Clear the string for the next input
-      return val;                     // Return the converted value
-    } else {
-      inputString += c;  // Add the character to the input string
-    }
+    if (c == '\n') {                  
+      int val = Uno.parseInt();
+      return val;                     
+    } 
   }
   return -1;  // Return -1 if no complete data is received
 }
 
 void DoNormalTraffic() {  // Correct function name
   long distanceA1 = getUltrasonicDistance(trigPinA1, echoPinA1);
-  long distanceB1 = getUltrasonicDistance(trigPinB1, echoPinB1);
-  long distanceC1 = getUltrasonicDistance(trigPinC1, echoPinC1);
-  long distanceD1 = getUltrasonicDistance(trigPinD1, echoPinD1);
-
   Serial.print("Distance A1: ");
-  Serial.print(distanceA1);
+  Serial.println(distanceA1);
+  roadOpen(pcf20, ledARed, ledAYellow, ledAGreen, distanceA1);
+
+  long distanceB1 = getUltrasonicDistance(trigPinB1, echoPinB1);
   Serial.print(" B1: ");
-  Serial.print(distanceB1);
+  Serial.println(distanceB1);
+  roadOpen(pcf20, ledBRed, ledBYellow, ledBGreen, distanceB1);
+
+  long distanceC1 = getUltrasonicDistance(trigPinC1, echoPinC1);
   Serial.print(" C1: ");
-  Serial.print(distanceC1);
+  Serial.println(distanceC1);
+  roadOpen(pcf21, ledCRed, ledCYellow, ledCGreen, distanceC1);
+
+  long distanceD1 = getUltrasonicDistance(trigPinD1, echoPinD1);
   Serial.print(" D1: ");
   Serial.println(distanceD1);
+  roadOpen(pcf21, ledDRed, ledDYellow, ledDGreen, distanceD1);
 
-  int distancelimit = 1000;  // Distance limit in cm
-
-  if (distanceA1 <= distancelimit) {
-    roadOpen(pcf20, ledARed, ledAYellow, ledAGreen, distanceA1);
-  }
-  if (distanceB1 <= distancelimit) {
-    roadOpen(pcf20, ledBRed, ledBYellow, ledBGreen, distanceB1);
-  }
-  if (distanceC1 <= distancelimit) {
-    roadOpen(pcf21, ledCRed, ledCYellow, ledCGreen, distanceC1);
-  }
-  if (distanceD1 <= distancelimit) {
-    roadOpen(pcf21, ledDRed, ledDYellow, ledDGreen, distanceD1);
-  }
 
   delay(1000);  // Delay for 1 second before the next measurement
 }
 
 void roadOpen(Adafruit_PCF8574 &pcf, uint8_t ledRed, uint8_t ledYellow, uint8_t ledGreen, long distance) {
-  pcf.digitalWrite(ledRed, LOW);
-  pcf.digitalWrite(ledGreen, HIGH);
-  delay(3000);  // Keep green on for 3 seconds
-  pcf.digitalWrite(ledGreen, LOW);
-  pcf.digitalWrite(ledYellow, HIGH);
-  delay(2000);  // Keep yellow on for 2 seconds
-  pcf.digitalWrite(ledYellow, LOW);
+  int receivedValue = ReceiveData();
+  if (receivedValue > 1) {
+    return;
+  }
+  
+  int delayTime = (distance <= 15 ) ? 7000 : 2000;
   pcf.digitalWrite(ledRed, HIGH);
+  pcf.digitalWrite(ledGreen, LOW);
+  delay(delayTime);
+  delay(3000);  // Keep green on for 3 seconds
+  pcf.digitalWrite(ledGreen, HIGH);
+  pcf.digitalWrite(ledYellow, LOW);
+  delay(2000);  // Keep yellow on for 2 seconds
+  pcf.digitalWrite(ledYellow, HIGH);
+  pcf.digitalWrite(ledRed, LOW);
   delay(1000);  // Keep red on for 1 second
 }
 
@@ -129,58 +158,54 @@ void controlLEDs(int val) {
   // Turn on or off LEDs based on the received value
   switch (val) {
     case 1:  // Turn on ledAGreen
-      pcf20.digitalWrite(ledARed, LOW);
-      pcf20.digitalWrite(ledAYellow, LOW);
-      pcf20.digitalWrite(ledAGreen, HIGH);
+      pcf20.digitalWrite(ledARed, HIGH);
+      pcf20.digitalWrite(ledAYellow, HIGH);
+      pcf20.digitalWrite(ledAGreen, LOW);
       break;
     case 2:  // Turn off ledAGreen
-      pcf20.digitalWrite(ledAGreen, LOW);
-      pcf20.digitalWrite(ledAYellow, HIGH);
-      delay(2000);
-      pcf20.digitalWrite(ledAYellow, LOW);
-      pcf20.digitalWrite(ledARed, HIGH);
+      pcf20.digitalWrite(ledBRed, HIGH);
+      pcf20.digitalWrite(ledBYellow, HIGH);
+      pcf20.digitalWrite(ledBGreen, LOW);
       break;
     case 3:  // Turn on ledBGreen
-      pcf20.digitalWrite(ledBRed, LOW);
-      pcf20.digitalWrite(ledBYellow, LOW);
-      pcf20.digitalWrite(ledBGreen, HIGH);
+      pcf21.digitalWrite(ledCRed, HIGH);
+      pcf21.digitalWrite(ledCYellow, HIGH);
+      pcf21.digitalWrite(ledCGreen, LOW);
       break;
     case 4:  // Turn off ledBGreen
-      pcf20.digitalWrite(ledBGreen, LOW);
-      pcf20.digitalWrite(ledBYellow, HIGH);
-      delay(2000);
-      pcf20.digitalWrite(ledBYellow, LOW);
-      pcf20.digitalWrite(ledBRed, HIGH);
-      break;
-    case 5:  // Turn on ledCGreen
-      pcf21.digitalWrite(ledCRed, LOW);
-      pcf21.digitalWrite(ledCYellow, LOW);
-      pcf21.digitalWrite(ledCGreen, HIGH);
-      break;
-    case 6:  // Turn off ledCGreen
-      pcf21.digitalWrite(ledCGreen, LOW);
-      pcf21.digitalWrite(ledCYellow, HIGH);
-      delay(2000);
-      pcf21.digitalWrite(ledCYellow, LOW);
-      pcf21.digitalWrite(ledCRed, HIGH);   // Use pcf21
-      break;
-    case 7:  // Turn on ledDGreen
-      pcf21.digitalWrite(ledDRed, LOW);
-      pcf21.digitalWrite(ledDYellow, LOW);
-      pcf21.digitalWrite(ledDGreen, HIGH);
-      break;
-    case 8:  // Turn off ledDGreen
-      pcf21.digitalWrite(ledDGreen, LOW);
+      pcf21.digitalWrite(ledDRed, HIGH);
       pcf21.digitalWrite(ledDYellow, HIGH);
-      delay(2000);
-      pcf21.digitalWrite(ledDYellow, LOW);
-      pcf21.digitalWrite(ledDRed, HIGH);   
+      pcf21.digitalWrite(ledDGreen, LOW);
+      break;
+    case 5:
+      enterSleepMode();
       break;
     default:
       Serial.println("Unknown command");
       break;
   }
 }
+
+void enterSleepMode() {
+  // Blink the yellow LED a few times before going to sleep
+    
+    pcf20.digitalWrite(ledAYellow, LOW);  // Turn the yellow LED on
+
+  // Enable the watchdog timer for sleep
+  wdt_enable(WDTO_2S); // Set the WDT timeout to 1 second
+  
+  // Sleep mode
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  sleep_enable(); // Enable sleep mode
+  sleep_cpu();    // Put the Arduino to sleep
+
+  // The processor will wake up here after the WDT timeout
+  sleep_disable(); // Disable sleep mode
+  wdt_reset(); // Reset the watchdog timer
+
+  pcf20.digitalWrite(ledAYellow, HIGH); 
+}
+
 
 long getUltrasonicDistance(uint8_t trigPin, uint8_t echoPin) {
   long duration, distance;
--- nodemcu/nodemcu.ino
+++ nodemcu/nodemcu.ino
@@ -2,69 +2,102 @@
 #define BLYNK_TEMPLATE_NAME "Checkpoint 2"
 #define BLYNK_AUTH_TOKEN "-ULiRAN0B-XcEMAx1Y9fBoaiAc2CzV0w"
 char ssid[] = "Hydrangeas "; // Your WiFi SSID
-char pass[] = "12345678";     // Your WiFi password
+char pass[] = "12345678";   // Your WiFi password
 
 #define BLYNK_PRINT Serial
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial NodeMCU(D2, D3); // RX | TX
+SoftwareSerial NodeMCU(D1, D2); // RX | TX
+
+bool v0State = false;
+bool v1State = false;
+bool v2State = false;
+bool v3State = false;
+bool v4State = false;
 
 void setup() {
-  Serial.begin(115200);
+  pinMode(D1, INPUT);
+  pinMode(D2, OUTPUT);
+  Serial.begin(9600);
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass); // Connect to Blynk
   NodeMCU.begin(9600);
 }
 
 BLYNK_WRITE(V0) {
   int value = param.asInt();  // Get the value from Blynk (0 or 1)
-  if (value == 1) {
+  if (value == 1 && !v1State && !v2State && !v3State && !v4State) {
     Serial.println("A is ON");
-    NodeMCU.println(1); // Send 'A' to Arduino
-  } else {
+    v0State = true;
+  } else if (value == 0 && v0State) {
     Serial.println("A is OFF");
-    NodeMCU.println(2); // Send 'a' to Arduino for OFF
+    v0State = false;
   }
-  delay(50); // Short delay for stability
 }
 
 BLYNK_WRITE(V1) {
   int value = param.asInt();
-  if (value == 1) {
+  if (value == 1 && !v0State && !v2State && !v3State && !v4State) {
     Serial.println("B is ON");
-    NodeMCU.println(3); // Send 'B' to Arduino
-  } else {
+    v1State = true;
+  } else if (value == 0 && v1State) {
     Serial.println("B is OFF");
-    NodeMCU.println(4); // Send 'b' to Arduino for OFF
+    v1State = false;
   }
-  delay(50);
 }
 
 BLYNK_WRITE(V2) {
   int value = param.asInt();
-  if (value == 1) {
+  if (value == 1 && !v0State && !v1State && !v3State && !v4State) {
     Serial.println("C is ON");
-    NodeMCU.println(5); // Send 'C' to Arduino
-  } else {
+    v2State = true;
+  } else if (value == 0 && v2State) {
     Serial.println("C is OFF");
-    NodeMCU.println(6); // Send 'c' to Arduino for OFF
+    v2State = false;
   }
-  delay(50);
 }
 
 BLYNK_WRITE(V3) {
   int value = param.asInt();
-  if (value == 1) {
+  if (value == 1 && !v0State && !v1State && !v2State && !v4State) {
     Serial.println("D is ON");
-    NodeMCU.println(7); // Send 'D' to Arduino
-  } else {
+    v3State = true;
+  } else if (value == 0 && v3State) {
     Serial.println("D is OFF");
-    NodeMCU.println(8); // Send 'd' to Arduino for OFF
+    v3State = false;
   }
-  delay(50);
+}
+
+BLYNK_WRITE(V4) {
+  int value = param.asInt();
+  if (value == 1 && !v0State && !v1State && !v2State && !v3State) {
+    Serial.println("Sleep mode is ON");
+    v4State = true;
+  } else if (value == 0 && v4State) {
+    Serial.println("Sleep mode is OFF");
+    v4State = false;
+  }
 }
 
 void loop() {
   Blynk.run(); // Run Blynk
+
+  if (v0State) {
+    NodeMCU.println(1); // Continuously send 'A' to Arduino while A is ON
+  }
+  if (v1State) {
+    NodeMCU.println(2); // Continuously send 'B' to Arduino while B is ON
+  }
+  if (v2State) {
+    NodeMCU.println(3); // Continuously send 'C' to Arduino while C is ON
+  }
+  if (v3State) {
+    NodeMCU.println(4); // Continuously send 'D' to Arduino while D is ON
+  }
+  if (v4State) {
+    NodeMCU.println(5); // Continuously send 'Sleep mode ON' to Arduino while sleep mode is ON
+  }
+
+  delay(1500); // Adjust delay for the rate of sending data
 }
